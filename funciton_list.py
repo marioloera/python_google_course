e62309850aa0ee55599b4afe2d21b9c8d567a864
Mario Loera Lozano <malo@lendify.se>
2020-03-21 21:36:03 +0100

    adding new files

--- funciton_list.py
+++ funciton_list.py
@@ -12,18 +12,11 @@ def some_other_fun():
 def main():
     checks = [
             (func1, 'error at func1'),
-            (some_other_fun, 'error at some_other_fun'),
+            (some_other_fun, 'error at some_other_fun')
             ]
 
     everything_ok = True
-
-    numbers = [23,392, 3920,]
-    numbers2 = [] # ok
-    numbers2 = [0, ] # ok
-    # numbers2 = [,] # not ok
-    print(numbers)
-    print(numbers2)
-
+    
     for check, msg in checks:
         if check():
             print(msg)
